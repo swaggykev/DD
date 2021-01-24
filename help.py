2400d0715a3c22f982a555db80eaaf4a710472b1
swaggykev <77936165+swaggykev@users.noreply.github.com>
2021-01-24 14:17:44 -0500

    Create help.py

---
+++ help.py
@@ -0,0 +1,3 @@
+def helppoint():
+  helping = 'Available Commands: \n***$gm*** : Assigns the gm role to member \n***$total <type of dice> <number of times dice is rolled> <additional number added>*** : Returns the total dice roll after rolling the dice x amount of times and adding the additional number \n(Available type of dice includes d4, d6, d8, d10, d12, d20, d100 where d represents dice and the number represents the number of faces\n***$characterstats*** : Return a randomized character stats '
+  return helping
